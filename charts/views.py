ed91f9c60463c35baf0543e12d5c915a63b184e0
Lionel <lionel_raja@cable.comcast.com>
2018-10-11 20:18:50 -0400

    first push

---
+++ charts/views.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.generic import View
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+User = get_user_model()
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'charts.html', {"customers": 10})
+
+
+
+def get_data(request, *args, **kwargs):
+    data = {
+        "sales": 100,
+        "customers": 10,
+    }
+    return JsonResponse(data) # http response
+
+class ChartData(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+        label = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
+        default_items = [1,3,4,3,6,7]
+        data = {
+            "lables": label,
+            "default_items": default_items,
+        }
+        return Response(data)
